d1072a351920c01108a329080e64f3dc1f1c149f
“Nick” <nkulway@gmail.com>
2024-08-19 20:26:54 -0400

    prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Updated import for React 18
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client' // Updated import for React 18
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Create root with React 18
+const root = ReactDOM.createRoot(document.getElementById('root')) // Create root with React 18
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
-reportWebVitals();
+reportWebVitals()
